a281e22d7e3a787343f8e8081585549f2422b8bd
Danil Deineka <tripledpadik@gmail.com>
2024-01-22 18:44:58 +0200

    Add routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import AddCompanyButton from './components/add-company-button';
-import ClientComponent from './components/client-component';
-import ServerComponent from './components/server-components';
-import ServerComponentCopy from './components/server-components-copy';
-import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from '@/app/components/add-company-button';
+import MagicButton from '@/app/components/magic-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <ServerComponent />
-      <ClientComponent>
-        <ServerComponentCopy />
-      </ClientComponent>
+      <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
